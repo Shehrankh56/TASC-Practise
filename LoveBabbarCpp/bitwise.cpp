11544dccaca038947fdf0761caf293ed68663087
Shehran56 <mohdshehrankhan@gmail.com>
2025-06-20 22:31:34 +0530

    Added bitwise cpp

---
+++ LoveBabbarCpp/bitwise.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=6;
+    /* int b=6;
+    cout<< "a & b :"<< (a&b)<< endl;
+    return 0; */
+    int ans= a>>1;
+    cout<< ans<< endl;
+}
