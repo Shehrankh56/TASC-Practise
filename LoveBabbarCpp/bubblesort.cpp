26268899d551c72f9f33d4da93120296e2e0e5ff
Shehran56 <mohdshehrankhan@gmail.com>
2025-07-21 09:47:37 +0530

    Added bubble sort

--- LoveBabbarCpp/bubblesort.cpp
+++ LoveBabbarCpp/bubblesort.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 void bubbleSort(int arr[], int n)
 {
-    for(int i=0; i< n ; i++)
+    for(int i=0; i< n-1 ; i++)
     {
-        if(arr[i]>arr[i+1])
-        {
-            swap(arr[i],arr[i+1]);
-        }
+       for(int j=0; j < n-i-1; j++){
+            if(arr[j] > arr[j+1] ){
+                swap(arr[j+1], arr[j]);
+            }
+       }
     }
 }
 int main()
